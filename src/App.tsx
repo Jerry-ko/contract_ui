99b5a4e98b1384b04a93016a480c8ffde9c5edc2
hannah <mjko8903@gmail.com>
2026-02-21 18:24:54 +0900

    화면 구조 1차 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Button, Flex } from "antd";
 import "./App.css";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <Flex vertical gap={20}>
+      {/* 파일 업로드 영역 */}
+      <section>
+        <Flex vertical gap={8} align="center">
+          <span>계약서를 업로드하세요</span>
+          <span>(PDF / 이미지)</span>
+          <Button>파일 선택</Button>
+        </Flex>
+      </section>
+
+      <section>[분석 결과 요약]</section>
+      <section>[위험 조항별 카드]</section>
+      <section>[한계 명시]</section>
+    </Flex>
+  );
 }
 
 export default App;
