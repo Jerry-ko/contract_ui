3623a5fd6ac41839e725bf025d627d2c7b46892f
hannah <mjko8903@gmail.com>
2026-02-22 16:55:44 +0900

    파일 업로드

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { Button, Flex } from "antd";
+import { Button, Flex, message, Upload, type UploadProps } from "antd";
 import "./App.css";
 
 function App() {
+  const props: UploadProps = {
+    name: "file",
+    action: "http://localhost:3000/api/analyze",
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
   return (
     <Flex vertical gap={20}>
       {/* 파일 업로드 영역 */}
@@ -9,7 +23,9 @@ function App() {
         <Flex vertical gap={8} align="center">
           <span>계약서를 업로드하세요</span>
           <span>(PDF / 이미지)</span>
-          <Button>파일 선택</Button>
+          <Upload {...props}>
+            <Button>파일 선택</Button>
+          </Upload>
         </Flex>
       </section>
 
